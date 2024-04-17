7fc94107fc2e3dd3b6dbaf9192f21ebbfb236537
DiegoDFB <difrancisblanco@gmail.com>
2024-04-17 15:35:00 -0300

    Trabalho Cara ou Coroa feito

---
+++ CoinFlip/CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GirarMoeda(object sender, EventArgs e)
+        {
+            Random Lado = new Random();
+            int SelecaoLado = Lado.Next(2);
+
+            if(SelecaoLado == 0)
+            {
+                CaraCoroa.Text = "Cara";
+            }
+            else
+            {
+                CaraCoroa.Text = "Coroa";
+            }
+
+            if (Escolha.SelectedIndex == SelecaoLado)
+            {
+                ApostaCerta.Text = "Sua aposta está certa!";
+            }
+            else
+            {
+                ApostaCerta.Text = "Sua aposta está errada!";
+            }
+
+
+        }
+    }
+
+}
